6db7a7b9430362f9e7bb4cc3b61aa0e43cd3dc27
samwattayakorn-oss <sam.wattayakorn@gmail.com>
2026-07-10 12:19:09 +0700

    Update saf_module_process.js

--- module/saf_module_process.js
+++ module/saf_module_process.js
@@ -1,19 +1,1 @@
-<script>
-function renderProducts() {
-  const container = document.getElementById("productContainer");
 
-  productData.forEach(item => {
-    const card = document.createElement("div");
-    card.className = "card";
-
-    card.innerHTML = `
-      <h3>${item.name}</h3>
-      <p>${item.desc}</p>
-    `;
-
-    container.appendChild(card);
-
-    animateCard(card);
-  });
-}
-</script>
